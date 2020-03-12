93cb9ca3497762193e78cbca784fbc32d34d3b6b
Gusgus1999 <55194877+Gusgus1999@users.noreply.github.com>
2020-03-12 07:47:51 -0600

    Add files via upload

---
+++ Progra_2020A_VariablesTypes/Progra_2020A_VariablesTypes/Progra_2020A_VariablesTypes.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+	cout << "Size of Char: " << sizeof(char) << " Byte " << endl;
+	cout << "Size of Unsigned Char: " << sizeof(unsigned char) << " Byte " << endl;
+	cout << "Size of Int: " << sizeof(int) << " Byte " << endl;
+	cout << "Size of Unsigned Int: " << sizeof(unsigned int) << " Byte " << endl;
+	cout << "Size of Short Int: " << sizeof(short int) << " Byte " << endl;
+	cout << "Size of Long Int: " << sizeof(long int) << " Byte " << endl;
+	cout << "Size of Signed Long Int: " << sizeof(signed long int) << " Byte " << endl;
+	cout << "Size of Unsigned Long Int: " << sizeof(unsigned long int) << " Byte " << endl;
+	cout << "Size of Long Long Int: " << sizeof(long long int) << " Byte " << endl;
+	cout << "Size of Unsigned Long Long Int: " << sizeof(long long int) << " Byte " << endl;
+	cout << "Size of Float: " << sizeof(float) << " Byte " << endl;
+	cout << "Size of  Double: " << sizeof(double) << " Byte " << endl;
+	cout << "Size of Long Double: " << sizeof(long double) << " Byte " << endl;
+	return 0;
+
+
+}
